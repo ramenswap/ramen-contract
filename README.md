a85836cee13ae45a8c9249f9122d5e7ead16e162
ramenswap <ramenswap8@gmail.com>
2021-02-15 02:18:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ramen Farming
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/e3050a18-1dda-4ba9-aa65-36a36cd4be94/deploy-status)](https://app.netlify.com/sites/ramenswap/deploys)
+
+https://ramenswap.finance. Feel free to read the code. More details coming soon.
+
+
---
+++ truffle-config.js
@@ -0,0 +1,44 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  
+  networks: {
+   development: {
+     host: "127.0.0.1",
+     port: 8545,
+     network_id: "*"
+   },
+   bsctest: {
+    provider: () => new HDWalletProvider(mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
+    network_id: 97,
+    confirmations: 5,
+    timeoutBlocks: 200,
+    skipDryRun: true,
+    production: true
+    
+   },
+   bsc: {
+    provider: () => new HDWalletProvider(mnemonic, 'https://bsc-dataseed.binance.org'),
+    network_id: 56,
+    confirmations: 10,
+    timeoutBlocks: 200,
+    skipDryRun: true
+   }
+  },  
+  compilers: {
+    solc: {
+      version: "0.6.12"
+    }
+  }
+};
