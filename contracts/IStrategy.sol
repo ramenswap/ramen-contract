ba1b7d8318889ea6e72242b10faf8027f8d757c6
Ramen Chef <78979398+ramenswap@users.noreply.github.com>
2021-03-04 23:36:28 +0700

    Create IStrategy.sol

---
+++ contracts/IStrategy.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+interface IStrategy {
+    function want() external view returns (address);
+    function deposit() external;
+    function withdraw(uint256) external;
+    function balanceOf() external view returns (uint256);
+    function harvest() external;
+    function retireStrat() external;
+}
